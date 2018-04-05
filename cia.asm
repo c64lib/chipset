1f73153833034c1d96408fe931023b6ff3fee07a
Maciej Malecki <maciej.malecki@capgemini.com>
2018-04-05 23:32:40 +0200

    All regs of CIA added, label for vic-2 renamed

--- cia.asm
+++ cia.asm
@@ -1,22 +1,56 @@
 #importonce
 .filenamespace c64lib
 
-.label CIA1_BASE        = $DC00 
-.label CIA2_BASE        = $DD00
-.label CIA1_DATA_PORT_A = CIA1_BASE + 0
-.label CIA1_DATA_PORT_B = CIA1_BASE + 1
-.label CIA2_DATA_PORT_A = CIA2_BASE + 0
+// CIA1
+.label CIA1               = $DC00 
+.label CIA1_DATA_PORT_A   = CIA1 + $00
+.label CIA1_DATA_PORT_B   = CIA1 + $01
+.label CIA1_DATA_DIR_A    = CIA1 + $02
+.label CIA1_DATA_DIR_B    = CIA1 + $03
+.label CIA1_TIMER_A_LO    = CIA1 + $04 
+.label CIA1_TIMER_A_HI    = CIA1 + $05 
+.label CIA1_TIMER_B_LO    = CIA1 + $06 
+.label CIA1_TIMER_B_HI    = CIA1 + $07 
+.label CIA1_TOD_SEC10     = CIA1 + $08 
+.label CIA1_TOD_SEC       = CIA1 + $09
+.label CIA1_TOD_MIN       = CIA1 + $0A
+.label CIA1_TOD_HOUR      = CIA1 + $0B
+.label CIA1_IO_BUFFER     = CIA1 + $0C
+.label CIA1_IRQ_CONTROL   = CIA1 + $0D
+.label CIA1_CONTROL_A     = CIA1 + $0E
+.label CIA1_CONTROL_B     = CIA1 + $0F
 
+// CIA2
+.label CIA2               = $DD00
+.label CIA2_DATA_PORT_A   = CIA2 + $00
+.label CIA2_DATA_PORT_B   = CIA2 + $01
+.label CIA2_DATA_DIR_A    = CIA2 + $02
+.label CIA2_DATA_DIR_B    = CIA2 + $03
+.label CIA2_TIMER_A_LO    = CIA2 + $04 
+.label CIA2_TIMER_A_HI    = CIA2 + $05 
+.label CIA2_TIMER_B_LO    = CIA2 + $06 
+.label CIA2_TIMER_B_HI    = CIA2 + $07 
+.label CIA2_TOD_SEC10     = CIA2 + $08 
+.label CIA2_TOD_SEC       = CIA2 + $09
+.label CIA2_TOD_MIN       = CIA2 + $0A
+.label CIA2_TOD_HOUR      = CIA2 + $0B
+.label CIA2_IO_BUFFER     = CIA2 + $0C
+.label CIA2_IRQ_CONTROL   = CIA2 + $0D
+.label CIA2_CONTROL_A     = CIA2 + $0E
+.label CIA2_CONTROL_B     = CIA2 + $0F
+
+// Joystick flags
 .label JOY_UP           = %00001
 .label JOY_DOWN         = %00010
 .label JOY_LEFT         = %00100
 .label JOY_RIGHT        = %01000
 .label JOY_FIRE         = %10000
 
-.label BANK_0         = %00000011
-.label BANK_1         = %00000010
-.label BANK_2         = %00000001
-.label BANK_3         = %00000000
+// VIC-II memory banks
+.label BANK_0           = %00000011
+.label BANK_1           = %00000010
+.label BANK_2           = %00000001
+.label BANK_3           = %00000000
 
 /*
  * Configures memory "bank" (16K) which is directly addressable by VIC2 chip.
